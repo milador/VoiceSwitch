faf7a460e827f9ebc98744506d4c8805e0f37764
Milad <milad.hajihassan@gmail.com>
2018-07-15 19:08:02 -0700

    Added the code

---
+++ Software/Wheelchair-VoiceControl/config.h
@@ -0,0 +1,18 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "Your Adafruit IO Username"
+#define IO_KEY         "Your Adafruit IO key"
+
+/******************************* WIFI **************************************/
+
+#define WIFI_SSID       "Your WIFI_SSID"
+#define WIFI_PASS       "Your WIFI_PASS"
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+
